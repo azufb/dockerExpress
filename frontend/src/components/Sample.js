f62e12b7f711a14262fd2b3163745241adc31fdf
azufb <curiousazunas@gmail.com>
2022-07-02 23:49:17 +0900

    エラーのため、一旦コメントアウト

--- frontend/src/components/Sample.js
+++ frontend/src/components/Sample.js
@@ -1,28 +1,22 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-const Sample = async () => {
-    const [message, setMessage] = useState('');
+const Sample = () => {
+    //const [message, setMessage] = useState('');
 
-    useEffect(() => {
-        const axiosFunc = async () => {
-            await axios.get('http://localhost:8080/api', {
-                headers: {
-                'Content-Type': 'application/json'
-                }
-            }).then(res => {
-                setMessage(res.data.message);
-            });
+    axios.get('http://localhost:8080/api', {
+        headers: {
+        'Content-Type': 'application/json'
         }
-
-        axiosFunc();
-    }, []);
+    }).then(res => {
+        //setMessage(res.data.message);
+    });
 
     return (
         <div>
-            <h1>message: {message}</h1>
+            <h1>message:</h1>
         </div>
-    )
+    );
 }
 
 export default Sample;
