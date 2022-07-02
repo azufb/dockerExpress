e932f23f9cd7d576df36d7bcaa0d4ffe81a435ff
azufb <curiousazunas@gmail.com>
2022-07-02 22:44:05 +0900

    docker-compose作成

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.NODE_DOCKER_PORT || 8080;
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+    res.setHeader("Access-Control-Max-Age", "3600");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, x-access-token, x-user-id,Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
