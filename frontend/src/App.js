bcb1ca2223a1ada351fe2fda7ba78b480dd36b7c
azufb <curiousazunas@gmail.com>
2022-07-16 23:02:35 +0900

    コンポーネントを追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
-import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
 const App = () => {
-  const [message, setMessage] = useState('aaa');
 
   return (
     <div className="App">
       <h1>こんにちは、世界。</h1>
-      <p>{message}</p>
       <Form />
+      <SignUp />
+      <SignIn />
     </div>
   );
 }
