69be7959ec706755e5edcc0a2f52ec022b0d162a
azufb <curiousazunas@gmail.com>
2022-08-30 19:50:37 +0900

    カテゴリのoptionが多いので、別ファイルで配列で管理するよう修正

--- frontend/src/components/ItemRegisterForm.jsx
+++ frontend/src/components/ItemRegisterForm.jsx
@@ -1,4 +1,5 @@
 import { useForm, useFieldArray } from "react-hook-form";
+import itemCategoryData from "../jsData/itemCategoryData";
 
 const ItemRegisterForm = () => {
     const { register, handleSubmit, control } = useForm({
@@ -14,10 +15,17 @@ const ItemRegisterForm = () => {
             ]
         }
     });
+
     const { fields, append } = useFieldArray({
         control,
         name: 'itemRegister'
     });
+
+    const appendForm = () => {
+        if (fields.length + 1 <= 10) {
+            return append({ itemName: '', itemPrice: '', itemType: '', itemCategory: '', comment: '' });
+        }
+    }
 
     return (
         <div>
@@ -39,33 +47,16 @@ const ItemRegisterForm = () => {
                             <label>カテゴリー:</label>
                             <select {...register(`itemRegister.${index}.itemCategory`)}>
                                 <option value='' selected>選択してください</option>
-                                <option value='toner'>拭き取り化粧水</option>
-                                <option value='lotion'>化粧水</option>
-                                <option value='faceMask'>フェイスマスク</option>
-                                <option value='emulsion'>乳液</option>
-                                <option value='serum'>美容液</option>
-                                <option value='cream'>クリーム</option>
-                                <option value='bb/cc'>BB/CCクリーム</option>
-                                <option value='primer'>下地</option>
-                                <option value='foundation'>ファンデーション</option>
-                                <option value='controlColor'>コントロールカラー</option>
-                                <option value='concealer'>コンシーラー</option>
-                                <option value='highlightingPowder'>ハイライト</option>
-                                <option value='blush'>チーク</option>
-                                <option value='lip'>口紅</option>
-                                <option value='eyebrow'>アイブロウ</option>
-                                <option value='eyeliner'>アイライナー</option>
-                                <option value='eyeShadow'>アイシャドウ</option>
-                                <option value='mascaraPrimer'>マスカラ下地</option>
-                                <option value='mascara'>マスカラ</option>
-                                <option value='other'>そのほか</option>
+                                {itemCategoryData.map((category, index) => (
+                                    <option key={index} value={category.value}>{category.name}</option>
+                                ))}
                             </select>
                             <label>コメント:</label>
                             <input {...register(`itemRegister.${index}.comment`)} />
                         </div>
                     ))}
                 </div>
-                <button type='button' onClick={() => append({ itemName: '', itemPrice: '', itemType: '', itemCategory: '', comment: '' })}>
+                <button type='button' onClick={appendForm}>
                     ＋
                 </button>
                 <input type="submit" />
---
+++ frontend/src/jsData/itemCategoryData.js
@@ -0,0 +1,84 @@
+const itemCategoryData = [
+    {
+        value: 'toner',
+        name: '拭き取り化粧水'
+    },
+    {
+        value: 'lotion',
+        name: '化粧水'
+    },
+    {
+        value: 'faceMask',
+        name: 'フェイスマスク'
+    },
+    {
+        value: 'emulsion',
+        name: '乳液'
+    },
+    {
+        value: 'serum',
+        name: '美容液'
+    },
+    {
+        value: 'cream',
+        name: 'クリーム'
+    },
+    {
+        value: 'bb/cc',
+        name: 'BB/CCクリーム'
+    },
+    {
+        value: 'primer',
+        name: '下地'
+    },
+    {
+        value: 'foundation',
+        name: 'ファンデーション'
+    },
+    {
+        value: 'controlColor',
+        name: 'コントロールカラー'
+    },
+    {
+        value: 'concealer',
+        name: 'コンシーラー'
+    },
+    {
+        value: 'highlightingPowder',
+        name: 'ハイライト'
+    },
+    {
+        value: 'blush',
+        name: 'チーク'
+    },
+    {
+        value: 'lip',
+        name: '口紅'
+    },
+    {
+        value: 'eyebrow',
+        name: 'アイブロウ'
+    },
+    {
+        value: 'eyeliner',
+        name: 'アイライナー'
+    },
+    {
+        value: 'eyeShadow',
+        name: 'アイシャドウ'
+    },
+    {
+        value: 'mascaraPrimer',
+        name: 'マスカラ下地'
+    },
+    {
+        value: 'mascara',
+        name: 'マスカラ'
+    },
+    {
+        value: 'other',
+        name: 'そのほか'
+    }
+]
+
+export default itemCategoryData;
