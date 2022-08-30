3763fdfb334096693a354eb1add169dcad726ba5
azufb <curiousazunas@gmail.com>
2022-08-30 13:31:05 +0900

    動き見るため、一旦コンポーネント追加

--- frontend/src/components/Authenticated.jsx
+++ frontend/src/components/Authenticated.jsx
@@ -1,7 +1,10 @@
+import ItemRegisterForm from "./ItemRegisterForm";
+
 const Authenticated = () => {
     return (
         <div>
             <h1>For Authenticated Member!</h1>
+            <ItemRegisterForm />
         </div>
     )
 }
