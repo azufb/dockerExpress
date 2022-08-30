3a88305957a7b26164f2bab3f4211d287572cb21
azufb <curiousazunas@gmail.com>
2022-08-30 09:49:57 +0900

    一覧表示処理作成

--- frontend/src/components/ItemList.jsx
+++ frontend/src/components/ItemList.jsx
@@ -1,6 +1,15 @@
+import { useRecoilValue } from "recoil";
+import { itemListAtom } from "../atoms/itemAtoms";
+
 const ItemList = () => {
+    const itemList = useRecoilValue(itemListAtom);
+
     return (
-        <div></div>
+        <div>
+            {itemList.map((item, index) => (
+                <p key={index}>{item.name}</p>
+            ))}
+        </div>
     );
 };
 
