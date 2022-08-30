d1b1887d5687e2271da440d6117d0cbab8194e70
azufb <curiousazunas@gmail.com>
2022-08-30 13:21:21 +0900

    ダイナミックフォームの実装

--- frontend/src/components/ItemRegisterForm.jsx
+++ frontend/src/components/ItemRegisterForm.jsx
@@ -1,22 +1,40 @@
+import { useForm, useFieldArray } from "react-hook-form";
+
 const ItemRegisterForm = () => {
+    const { register, handleSubmit, control } = useForm();
+    const { fields, append } = useFieldArray({
+        control,
+        name: 'itemRegister'
+    });
+
     return (
         <div>
-            <h1>フォーム</h1>
-            <div>
-                <label>商品名:</label>
-                <input name='itemName' />
-                <label>価格:</label>
-                <input name='itemPrice' />
-                <label>分類:</label>
-                <select name='itemType'>
-                    <option value='現品' selected>現品</option>
-                    <option value='サンプル'>サンプル</option>
-                </select>
-                <label>カテゴリー:</label>
-                <input name='itemCategory' />
-                <label>コメント:</label>
-                <input name='comment' />
-            </div>
+            <h1>商品登録フォーム</h1>
+            <form onSubmit={ handleSubmit() }>
+                <div>
+                    {fields.map((form, index) => (
+                        <div key={index}>
+                            <label>商品名:</label>
+                            <input {...register(`itemRegister.${index}.itemName`)} />
+                            <label>価格:</label>
+                            <input {...register(`itemRegister.${index}.itemPrice`)} />
+                            <label>分類:</label>
+                            <select {...register(`itemRegister.${index}.itemType`)}>
+                                <option value='actual' selected>現品</option>
+                                <option value='sample'>サンプル</option>
+                            </select>
+                            <label>カテゴリー:</label>
+                            <input {...register(`itemRegister.${index}.itemCategory`)} />
+                            <label>コメント:</label>
+                            <input {...register(`itemRegister.${index}.comment`)} />
+                        </div>
+                    ))}
+                </div>
+                <button type='button' onClick={() => append({ itemName: '', itemPrice: '', itemType: 'actual', itemCategory: '', comment: '' })}>
+                    ＋
+                </button>
+                <input type="submit" />
+            </form>
         </div>
     );
 };
