c9d6842ed98e7909f46fc89dd910c115b0a1c5f1
azufb <curiousazunas@gmail.com>
2022-08-30 11:50:07 +0900

    商品登録で使うフォームの作成

--- frontend/src/components/ItemRegisterForm.jsx
+++ frontend/src/components/ItemRegisterForm.jsx
@@ -1,10 +1,22 @@
-import { useForm } from 'react-hook-form';
-
 const ItemRegisterForm = () => {
-    const { register, handleSubmit } = useForm();
     return (
         <div>
-            <h1>商品登録フォーム</h1>
+            <h1>フォーム</h1>
+            <div>
+                <label>商品名:</label>
+                <input name='itemName' />
+                <label>価格:</label>
+                <input name='itemPrice' />
+                <label>分類:</label>
+                <select name='itemType'>
+                    <option value='現品' selected>現品</option>
+                    <option value='サンプル'>サンプル</option>
+                </select>
+                <label>カテゴリー:</label>
+                <input name='itemCategory' />
+                <label>コメント:</label>
+                <input name='comment' />
+            </div>
         </div>
     );
 };
