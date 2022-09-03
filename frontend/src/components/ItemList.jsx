a2c16b01e95ae239982a7a730773cec876490a41
azufb <curiousazunas@gmail.com>
2022-09-03 09:58:43 +0900

    不要なimport等削除

--- frontend/src/components/ItemList.jsx
+++ frontend/src/components/ItemList.jsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
-import { useRecoilValue, useSetRecoilState, useRecoilState } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 import { userInfoAtom } from "../atoms/userInfoAtom";
 import { itemListAtom } from "../atoms/itemAtom";
-import itemSelector from "../selectors/itemSelector";
 import axios from 'axios';
 
 const ItemList = () => {
     const userId = useRecoilValue(userInfoAtom);
     const [itemList, setItemList] = useRecoilState(itemListAtom);
-    //const itemList = useRecoilValue(itemSelector);
 
     useEffect(() => {
         const param = {
@@ -38,19 +36,26 @@ const ItemList = () => {
     return (
         <div>
             <div>一覧</div>
-            <div>
-                {itemList.length !== 0 ? (itemList.map((item, index) => (
-                    <div key={index}>
-                        <p>{item.itemName}</p>
-                        <p>{item.itemPrice}</p>
-                        <p>{item.itemType}</p>
-                        <p>{item.itemCategory}</p>
-                        <p>{item.comment}</p>
-                    </div>
-                ))) :(
-                    <p>何もアイテムがないよ。</p>
-                )}
-            </div>
+            <table>
+                <thead>
+                    <th>商品名</th>
+                    <th>価格</th>
+                    <th>タイプ</th>
+                    <th>商品カテゴリ</th>
+                    <th>コメント</th>
+                </thead>
+                <tbody>
+                {itemList.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.itemName}</td>
+                        <td>{item.itemPrice}</td>
+                        <td>{item.itemType}</td>
+                        <td>{item.itemCategory}</td>
+                        <td>{item.comment}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     );
 };
