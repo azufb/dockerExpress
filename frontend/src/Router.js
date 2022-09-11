3cb528a380b817251798268062ada84404cff012
azufb <curiousazunas@gmail.com>
2022-09-11 16:57:37 +0900

    ルーティング修正

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -3,6 +3,9 @@ import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import Authenticated from './components/Authenticated';
+import ItemDetail from './components/ItemDetail';
+import ItemList from './components/ItemList';
+import ItemRegisterForm from './components/ItemRegisterForm';
 
 const Router = () => {
     return (
@@ -11,7 +14,11 @@ const Router = () => {
                 <Route index element={<Home />} />
                 <Route path='SignUp' element={<SignUp />} />
                 <Route path='SignIn' element={<SignIn />} />
-                <Route path='Authenticated/:userId' element={<Authenticated />} />
+                <Route path='Authenticated/:userId' element={<Authenticated />}>
+                    <Route path='ItemList' element={<ItemList />} />
+                    <Route path='ItemRegisterForm' element={<ItemRegisterForm />} />
+                    <Route path='ItemDetail' element={<ItemDetail />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
