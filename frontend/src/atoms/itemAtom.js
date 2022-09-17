54b328c0656888d814501d10564beff1d00d9bbb
azufb <curiousazunas@gmail.com>
2022-09-17 22:38:25 +0900

    商品詳細で表示するためのatom作成

--- frontend/src/atoms/itemAtom.js
+++ frontend/src/atoms/itemAtom.js
@@ -4,3 +4,8 @@ export const itemListAtom = atom({
     key: 'itemList', // keyには一意の名称を入れる。
     default: []
 });
+
+export const filteredItemAtom = atom({
+    key: 'filteredItemAtom',
+    default: []
+});
