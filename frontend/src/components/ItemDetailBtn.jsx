1e27b57dbe878636a1ab3c63c85c8a342ca78c71
azufb <curiousazunas@gmail.com>
2022-09-17 22:38:55 +0900

    商品詳細遷移ボタン修正

--- frontend/src/components/ItemDetailBtn.jsx
+++ frontend/src/components/ItemDetailBtn.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 const ItemDetailBtn = (props) => {
+    const navigate = useNavigate();
+
+    const moveToItemDetailPage = () => {
+        navigate(`../ItemDetail/${props.itemId}`);
+    };
+
     return (
-        <button>商品詳細</button>
+        <button onClick={moveToItemDetailPage}>商品詳細</button>
     );
 };
 
