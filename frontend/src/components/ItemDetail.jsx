5bce1c0618372392d636795bcdf341f11db1d308
azufb <curiousazunas@gmail.com>
2022-09-17 22:38:40 +0900

    商品詳細コンポーネント修正

--- frontend/src/components/ItemDetail.jsx
+++ frontend/src/components/ItemDetail.jsx
@@ -1,17 +1,39 @@
-const ItemDetail = (props) => {
+import { useRecoilValue, useRecoilState } from "recoil";
+import { useParams } from "react-router-dom";
+import { itemListAtom, filteredItemAtom } from "../atoms/itemAtom";
+import { useEffect } from "react";
+
+const ItemDetail = () => {
+    const items = useRecoilValue(itemListAtom);
+    const [filteredItem, setFilteredItem] = useRecoilState(filteredItemAtom);
+    const paramObj = useParams();
+    const itemId = paramObj.itemId;
+
+    useEffect(() => {
+        const filtered = items.filter((item) => {
+            return item.itemId === itemId;
+        });
+
+        setFilteredItem(filtered);
+    }, []);
+
     return (
         <div>
             <h1>商品詳細</h1>
-            <label>商品名</label>
-            <p>{props.item.itemName}</p>
-            <label>価格</label>
-            <p>{props.item.itemPrice}</p>
-            <label>商品タイプ</label>
-            <p>{props.item.itemType}</p>
-            <label>カテゴリ</label>
-            <p>{props.item.itemCategory}</p>
-            <label>コメント</label>
-            <p>{props.item.comment}</p>
+            {filteredItem.map((item) => (
+                <>
+                    <label>商品名</label>
+                    <p>{item.itemName}</p>
+                    <label>価格</label>
+                    <p>{item.itemPrice}</p>
+                    <label>商品タイプ</label>
+                    <p>{item.itemType}</p>
+                    <label>カテゴリ</label>
+                    <p>{item.itemCategory}</p>
+                    <label>コメント</label>
+                    <p>{item.comment}</p>
+                </>
+            ))}
         </div>
     );
 };
