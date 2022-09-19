8e5c9ba413933c5431e2877762e5af62619ac7d5
azufb <curiousazunas@gmail.com>
2022-09-19 17:58:13 +0900

    編集画面に渡す値をuseRefを使って取得

--- frontend/src/components/ItemDetail.jsx
+++ frontend/src/components/ItemDetail.jsx
@@ -1,40 +1,63 @@
-import { useRecoilValue } from "recoil";
+import { useRef } from 'react';
+import { useRecoilValue, useRecoilState } from "recoil";
 import { useParams } from "react-router-dom";
 import { itemListAtom } from "../atoms/itemAtom";
 import { isOpenModalsAtom } from '../atoms/isOpenModals';
-import EditItemBtn from './EditItemBtn';
 import EditItem from './EditItem';
 
 const ItemDetail = () => {
     const items = useRecoilValue(itemListAtom);
-    const isOpenEditModal = useRecoilValue(isOpenModalsAtom);
+    const [isOpenEditModal, setIsOpenEditItemModal] = useRecoilState(isOpenModalsAtom);
     const paramObj = useParams();
     const itemId = paramObj.itemId;
+    const refItemName = useRef('');
+    const refItemPrice = useRef('');
+    const refItemType = useRef('');
+    const refItemCategory = useRef('');
+    const refItemComment = useRef('');
+    let defaultItemName = refItemName.current.innerText;
+    let defaultItemPrice = refItemPrice.current.innerText;
+    let defaultItemType = refItemType.current.innerText;
+    let defaultItemCategory = refItemCategory.current.innerText;
+    let defaultComment = refItemComment.current.innerText;
 
+    const openEditItemModal = () => {
+        const paramBool = {
+            editItemModal: true
+        };
+
+        setIsOpenEditItemModal(paramBool);
+    };
 
     return (
         <div>
             <h1>商品詳細</h1>
             <div>
-                <EditItemBtn />
+                <button onClick={openEditItemModal}>編集</button>
             </div>
             {items.filter((list) => list.itemId === parseInt(itemId)).map((item) => (
                 <>
                     <label>商品名</label>
-                    <p>{item.itemName}</p>
+                    <p ref={refItemName}>{item.itemName}</p>
                     <label>価格</label>
-                    <p>{item.itemPrice}</p>
+                    <p ref={refItemPrice}>{item.itemPrice}</p>
                     <label>商品タイプ</label>
-                    <p>{item.itemType}</p>
+                    <p ref={refItemType}>{item.itemType}</p>
                     <label>カテゴリ</label>
-                    <p>{item.itemCategory}</p>
+                    <p ref={refItemCategory}>{item.itemCategory}</p>
                     <label>コメント</label>
-                    <p>{item.comment}</p>
+                    <p ref={refItemComment}>{item.comment}</p>
                 </>
             ))}
             <div>
                 {isOpenEditModal.editItemModal && (
-                    <EditItem />
+                    <EditItem
+                        defaultItemName={defaultItemName}
+                        defaultItemPrice={defaultItemPrice}
+                        defaultItemType={defaultItemType}
+                        defaultItemCategory={defaultItemCategory}
+                        defaultComment={defaultComment}
+                    />
                 )}
             </div>
         </div>
