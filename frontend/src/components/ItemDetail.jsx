ffc7889d9cdbb9510eda95523db7ec5e330ff903
azufb <curiousazunas@gmail.com>
2022-09-19 23:00:55 +0900

    商品詳細の表示と編集画面に渡すデフォ値をPOSTで取得した値を使うよう修正

--- frontend/src/components/ItemDetail.jsx
+++ frontend/src/components/ItemDetail.jsx
@@ -1,25 +1,47 @@
-import { useRef } from 'react';
-import { useRecoilValue, useRecoilState } from "recoil";
+import { useEffect } from 'react';
+import { useRecoilState } from "recoil";
 import { useParams } from "react-router-dom";
-import { itemListAtom } from "../atoms/itemAtom";
+import { itemDetailAtom } from "../atoms/itemAtom";
 import { isOpenModalsAtom } from '../atoms/isOpenModals';
 import EditItem from './EditItem';
+import axios from 'axios';
 
 const ItemDetail = () => {
-    const items = useRecoilValue(itemListAtom);
     const [isOpenEditModal, setIsOpenEditItemModal] = useRecoilState(isOpenModalsAtom);
+    const [itemDetail, setItemDetail] = useRecoilState(itemDetailAtom);
     const paramObj = useParams();
-    const itemId = paramObj.itemId;
-    const refItemName = useRef('');
-    const refItemPrice = useRef('');
-    const refItemType = useRef('');
-    const refItemCategory = useRef('');
-    const refItemComment = useRef('');
-    let defaultItemName = refItemName.current.innerText;
-    let defaultItemPrice = refItemPrice.current.innerText;
-    let defaultItemType = refItemType.current.innerText;
-    let defaultItemCategory = refItemCategory.current.innerText;
-    let defaultComment = refItemComment.current.innerText;
+
+    useEffect(() => {
+        const getItem = async () => {
+            let resData;
+
+            const param = {
+                userId: paramObj.userId,
+                itemId: paramObj.itemId
+            }
+            await axios
+            .post('http://localhost:6868/getItem', param)
+            .then((res) => {
+                console.log(res.data.response);
+                resData = res.data
+            });
+
+            if (resData.status === 200) {
+                const getItemParam = {
+                    itemName: resData.response.itemName,
+                    itemPrice: resData.response.itemPrice,
+                    itemType: resData.response.itemType,
+                    itemCategory: resData.response.itemCategory,
+                    comment: resData.response.comment
+                }
+
+                setItemDetail(getItemParam);
+            }
+
+        };
+
+        getItem();
+    }, [paramObj.itemId, paramObj.userId, setItemDetail]);
 
     const openEditItemModal = () => {
         const paramBool = {
@@ -35,28 +57,25 @@ const ItemDetail = () => {
             <div>
                 <button onClick={openEditItemModal}>編集</button>
             </div>
-            {items.filter((list) => list.itemId === parseInt(itemId)).map((item) => (
-                <>
-                    <label>商品名</label>
-                    <p ref={refItemName}>{item.itemName}</p>
-                    <label>価格</label>
-                    <p ref={refItemPrice}>{item.itemPrice}</p>
-                    <label>商品タイプ</label>
-                    <p ref={refItemType}>{item.itemType}</p>
-                    <label>カテゴリ</label>
-                    <p ref={refItemCategory}>{item.itemCategory}</p>
-                    <label>コメント</label>
-                    <p ref={refItemComment}>{item.comment}</p>
-                </>
-            ))}
+            <label>商品名</label>
+            <p>{itemDetail.itemName}</p>
+            <label>価格</label>
+            <p>{itemDetail.itemPrice}</p>
+            <label>商品タイプ</label>
+            <p>{itemDetail.itemType}</p>
+            <label>カテゴリ</label>
+            <p>{itemDetail.itemCategory}</p>
+            <label>コメント</label>
+            <p>{itemDetail.comment}</p>
+
             <div>
                 {isOpenEditModal.editItemModal && (
                     <EditItem
-                        defaultItemName={defaultItemName}
-                        defaultItemPrice={defaultItemPrice}
-                        defaultItemType={defaultItemType}
-                        defaultItemCategory={defaultItemCategory}
-                        defaultComment={defaultComment}
+                        defaultItemName={itemDetail.itemName}
+                        defaultItemPrice={itemDetail.itemPrice}
+                        defaultItemType={itemDetail.itemType}
+                        defaultItemCategory={itemDetail.itemCategory}
+                        defaultComment={itemDetail.comment}
                     />
                 )}
             </div>
