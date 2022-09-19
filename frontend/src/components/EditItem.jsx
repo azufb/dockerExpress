a56d1f277a0dc9c6563b027f345fe790439bbe8f
azufb <curiousazunas@gmail.com>
2022-09-19 17:58:27 +0900

    フォームに初期値を入れておく

--- frontend/src/components/EditItem.jsx
+++ frontend/src/components/EditItem.jsx
@@ -1,29 +1,41 @@
+import { useForm } from "react-hook-form";
 import itemCategoryData from "../jsData/itemCategoryData";
 
-const EditItem = () => {
+const EditItem = (props) => {
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            itemName: props.defaultItemName,
+            itemPrice: props.defaultItemPrice,
+            itemType: props.defaultItemType,
+            itemCategory: props.defaultItemCategory,
+            comment: props.defaultComment
+        }
+    });
+
+    console.log(props);
     return (
         <>
             <h1>商品編集</h1>
             <form>
                 <label>商品名:</label>
-                <input />
+                <input {...register('itemName')} />
                 <label>価格:</label>
-                <input />
+                <input {...register('itemPrice')} />
                 <label>分類:</label>
-                <select>
+                <select {...register('itemType')}>
                     <option value='' selected>選択してください</option>
                     <option value='actual'>現品</option>
                     <option value='sample'>サンプル</option>
                 </select>
                 <label>カテゴリー:</label>
-                <select>
+                <select {...register('itemCategory')}>
                     <option value='' selected>選択してください</option>
                     {itemCategoryData.map((category, index) => (
                         <option key={index} value={category.value}>{category.name}</option>
                     ))}
                 </select>
                 <label>コメント:</label>
-                <input />
+                <input {...register('comment')} />
             </form>
         </>
     );
