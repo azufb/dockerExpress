37bda8337f31681db12fe3e0fc24eca7e581ff7f
azufb <curiousazunas@gmail.com>
2022-09-23 14:21:01 +0900

    Chakra UI導入

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
