3166c99e91d4967a1d5860d2d214f91d90f7c526
azufb <curiousazunas@gmail.com>
2022-10-12 15:34:09 +0900

    使用期限の選択肢を用意

---
+++ frontend/src/jsData/customItemUseDeadlineOptions.js
@@ -0,0 +1,148 @@
+const customItemUseDeadlineOptions = [
+    {
+        value: 1,
+        data: ''
+    },
+    {
+        value: 2,
+        data: ''
+    },
+    {
+        value: 3,
+        data: ''
+    },
+    {
+        value: 4,
+        data: ''
+    },
+    {
+        value: 5,
+        data: ''
+    },
+    {
+        value: 6,
+        data: ''
+    },
+    {
+        value: 7,
+        data: ''
+    },
+    {
+        value: 8,
+        data: ''
+    },
+    {
+        value: 9,
+        data: ''
+    },
+    {
+        value: 10,
+        data: ''
+    },
+    {
+        value: 11,
+        data: ''
+    },
+    {
+        value: 12,
+        data: '1年'
+    },
+    {
+        value: 13,
+        data: ''
+    },
+    {
+        value: 14,
+        data: ''
+    },
+    {
+        value: 15,
+        data: ''
+    },
+    {
+        value: 16,
+        data: ''
+    },
+    {
+        value: 17,
+        data: ''
+    },
+    {
+        value: 18,
+        data: ''
+    },
+    {
+        value: 19,
+        data: ''
+    },
+    {
+        value: 20,
+        data: ''
+    },
+    {
+        value: 21,
+        data: ''
+    },
+    {
+        value: 22,
+        data: ''
+    },
+    {
+        value: 23,
+        data: ''
+    },
+    {
+        value: 24,
+        data: '2年'
+    },
+    {
+        value: 25,
+        data: ''
+    },
+    {
+        value: 26,
+        data: ''
+    },
+    {
+        value: 27,
+        data: ''
+    },
+    {
+        value: 28,
+        data: ''
+    },
+    {
+        value: 29,
+        data: ''
+    },
+    {
+        value: 30,
+        data: ''
+    },
+    {
+        value: 31,
+        data: ''
+    },
+    {
+        value: 32,
+        data: ''
+    },
+    {
+        value: 33,
+        data: ''
+    },
+    {
+        value: 34,
+        data: ''
+    },
+    {
+        value: 35,
+        data: ''
+    },
+    {
+        value: 36,
+        data: '3年'
+    },
+];
+
+export default customItemUseDeadlineOptions;
