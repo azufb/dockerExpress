3eb89600f67f3c3918274ce1d7e2c11740185f89
azufb <curiousazunas@gmail.com>
2022-10-22 21:22:20 +0900

    ユーザ情報更新ページ作成

--- frontend/src/components/EditUserData.jsx
+++ frontend/src/components/EditUserData.jsx
@@ -2,9 +2,19 @@ import { useForm } from 'react-hook-form';
 import { Input } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { userState } from "../atoms/userAtom";
+import { useSetRecoilState } from 'recoil';
 
-const EditUserData = () => {
-    const { register, handleSubmit } = useForm();
+const EditUserData = (props) => {
+    const setUserData = useSetRecoilState(userState);
+
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            name: props.defaultName,
+            email: props.defaultEmail,
+            password: ''
+        }
+    });
     const paramObj = useParams();
 
     const updateUserData = async (data) => {
@@ -15,11 +25,23 @@ const EditUserData = () => {
             password: data.password
         }
 
+        let resData = undefined;
+
         await axios
         .post('http://localhost:6868/updateUserData', param)
         .then((res) => {
             console.log(res);
-        })
+            resData = res.data;
+        });
+
+        if (resData.status === 200) {
+            const setUserDataParam = {
+                name: data.name,
+                email: data.email
+            };
+
+            setUserData(setUserDataParam);
+        }
     }
     return (
         <div>
