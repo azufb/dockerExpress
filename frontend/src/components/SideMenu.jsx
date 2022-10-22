c34a7a9a75a6757774fd8d655e54c01d6819acb4
azufb <curiousazunas@gmail.com>
2022-10-22 21:21:41 +0900

    メニュー修正

--- frontend/src/components/SideMenu.jsx
+++ frontend/src/components/SideMenu.jsx
@@ -7,6 +7,7 @@ const SideMenu = () => {
                 <li>
                     <Link to='ItemList'>一覧</Link>
                     <Link to='ItemRegisterForm'>登録</Link>
+                    <Link to='UserData'>ユーザ情報</Link>
                 </li>
             </ul>
         </nav>
